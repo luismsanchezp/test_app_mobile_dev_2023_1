a5dc37abef136c3c0acfb7ee65f1175f9a38311f
Luis Miguel Sanchez Pinilla <luism.sanchezp@autonoma.edu.co>
2023-02-07 21:56:11 -0500

    Changes to User Registration form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,17 +7,18 @@ import {
     Text, 
     TextInput, 
     View,
+    Pressable
 } from 'react-native'
-import DatePicker from 'react-native-date-picker';
+import DatePicker from 'react-native-modern-datepicker';
 import { RadioButton } from 'react-native-paper';
 
 export const Form = ({modalVisibleForm}) => {
-    const [userName, setUserName] = useState('Your Name');
-    const [userSurname, setUserSurname] = useState('Your Surname');
-    const [userEmail, setUserEmail] = useState('name@email.com');
-    const [dateBirth, setDateBirth] = useState(new Date());
-    const [userGender, setUserGender] = useState('F');
-    
+    const [userName, setUserName] = useState('');
+    const [userSurname, setUserSurname] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [dateBirth, setDateBirth] = useState('');
+    const [userGender, setUserGender] = useState('');
+
     console.log('userName: ', userName);
     console.log('userSurname: ', userSurname);
     console.log('userEmail: ', userEmail);
@@ -28,79 +29,83 @@ export const Form = ({modalVisibleForm}) => {
         <Modal animationType='slide' visible={modalVisibleForm}>
             <SafeAreaView style={styles.content}>
                 <ScrollView>
-                    <Text style={styles.formTitle}>Form</Text>
-                    <View>
-                        <Text style={styles.textTitle}
-                        >Name</Text>
-                        <TextInput
-                            placeholderTextColor={'#203474'}
-                            autoCapitalize='words'
-                            autoCorrect={false}
-                            value={userName}
-                            onChangeText={setUserName}
-                            style={styles.textBox}
-                        ></TextInput>
-                        <Text style={styles.textTitle}
-                        >Surname</Text>
-                        <TextInput
-                            placeholderTextColor={'#203474'}
-                            autoCapitalize='words'
-                            autoCorrect={false}
-                            value={userSurname}
-                            onChangeText={setUserSurname}
-                            style={styles.textBox}
-                        ></TextInput>
-                        <View
-                            style={styles.containerGender}
-                        >
-                            <Text
-                                style={styles.textTitle}
-                            >Gender</Text>
+                    <Text 
+                        style={styles.formTitle}
+                    >Form</Text>
+                    <Text 
+                        style={styles.textTitle}
+                    >Name</Text>
+                    <TextInput
+                        placeholder='Your name'
+                        autoCapitalize='words'
+                        autoCorrect={false}
+                        value={userName}
+                        onChangeText={setUserName}
+                        style={styles.textBox}
+                    ></TextInput>
+                    <Text 
+                        style={styles.textTitle}
+                    >Surname</Text>
+                    <TextInput
+                        placeholder='Your surname'
+                        autoCapitalize='words'
+                        autoCorrect={false}
+                        value={userSurname}
+                        onChangeText={setUserSurname}
+                        style={styles.textBox}
+                    ></TextInput>
+                    <View style={styles.containerGender}>
+                        <Text
+                            style={styles.textTitle}
+                        >Gender</Text>
+                        <View style={styles.containerGenderRadioButtons}>
                             <RadioButton
-                                value="Female"
+                                color='#92b5be'
+                                value="F"
                                 status={ userGender === 'F' ? 'checked' : 'unchecked' }
                                 onPress={() => setUserGender('F')}
                             />
+                            <Text>Female</Text>
+                        </View>
+                        <View style={styles.containerGenderRadioButtons}>
                             <RadioButton
-                                value="Male"
+                                color='#92b5be'
+                                value="M"
                                 status={ userGender === 'M' ? 'checked' : 'unchecked' }
                                 onPress={() => setUserGender('M')}
                             />
+                            <Text>Male</Text>
                         </View>
-                        <Text
-                            style={styles.textTitle}
-                        >Birth Date</Text>
-                        <DatePicker
-                            date={dateBirth}
-                            locale='en_US'
-                            mode='date'
-                            minimumDate={new Date(1900, 0, 1)}
-                            maximumDate={new Date()}
-                            onDateChange={setDateBirth}
-                        />
-                        <Text style={styles.textTitle}
-                        >Email</Text>
-                        <TextInput
-                            placeholderTextColor={'#203474'}
-                            autoCapitalize='none'
-                            autoCorrect={false}
-                            autoCompleteType='email'
-                            value={userEmail}
-                            onChangeText={setUserEmail}
-                            style={styles.textBox}
-                        ></TextInput>
-                        <View
-                            style={styles.containerButtons}
-                        >
-                            <Pressable
-                                style={styles.btnStyle2}>
-                                <Text style={styles.btnTxtStyle}>Aceptar</Text>
-                            </Pressable>
-                            <Pressable
-                                style={styles.btnStyle2}>
-                                <Text style={styles.btnTxtStyle}>Cancelar</Text>
-                            </Pressable>
-                        </View>
+                    </View>
+                    <Text
+                        style={styles.textTitle}
+                    >Birth Date</Text>
+                    <DatePicker
+                        options={styles.datePicker}
+                        minimumDate='1900/01/01'
+                        mode='calendar'
+                        onSelectedChange={date => setDateBirth(date)}
+                    />
+                    <Text style={styles.textTitle}
+                    >Email</Text>
+                    <TextInput
+                        placeholder='name@email.com'
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        autoCompleteType='email'
+                        value={userEmail}
+                        onChangeText={setUserEmail}
+                        style={styles.textBox}
+                    ></TextInput>
+                    <View style={styles.containerButtons}>
+                        <Pressable
+                            style={styles.btnStyle2}>
+                            <Text style={styles.btnTxtStyle}>Aceptar</Text>
+                        </Pressable>
+                        <Pressable
+                            style={styles.btnStyle2}>
+                            <Text style={styles.btnTxtStyle}>Cancelar</Text>
+                        </Pressable>
                     </View>
                 </ScrollView>
             </SafeAreaView>
@@ -113,21 +118,31 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginHorizontal: 16,
+        marginTop: 20,
+        marginBottom: 20,
     },
     containerGender: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
+        marginBottom: 10,
+    },
+    containerGenderRadioButtons: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        marginBottom: 10,
     },
     containerButtons: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-evenly',
         alignItems: 'center',
     },
     formTitle: {
         fontSize: 40,
         fontWeight: 'bold',
         marginBottom: 20,
+        textAlign: 'center',
     },
     textTitle: {
         fontSize: 20,
@@ -135,17 +150,27 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     textBox: {
-        borderColor: '#203474',
+        borderColor: '#92b5be',
         borderWidth: 1,
         borderRadius: 10,
         padding: 10,
         marginBottom: 20,
     },
     btnStyle2: {
-        backgroundColor: '#EA4918',
-        padding: 20,
-        marginTop: 20,
+        backgroundColor: '#92b5be',
+        padding: 15,
         marginHorizontal: 10,
         borderRadius: 10,
+        width: 120,
+    },
+    btnTxtStyle: {
+        textAlign: 'center',
+        color: '#fff',
+        fontWeight: 'bold',
+        textTransform: 'capitalize',
+    },
+    datePicker: {
+        selectedTextColor: '#fff',
+        mainColor: '#92b5be',
     },
 });
--- src/components/NewForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const NewForm = () => {
-  return (
-    <div>NewForm</div>
-  )
-}
